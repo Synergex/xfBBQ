9a0aeb4f0e8a63b934d52d571027c202b9603803
Johnson Luong <johnson.luong@synergex.com>
2019-10-21 12:01:57 -0700

    replaced home link with xfbbq link

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,41 +6,30 @@ const Header = () => {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <div className="navbar-brand">
+      <NavLink to="/" exact className="navbar-brand">
         <i>xf</i>BBQ
-      </div>
-      <div id="navbarColor01" style={{}}>
-        <ul className="navbar-nav mr-auto">
-          <li className="navbar-item">
-            <NavLink
-              to="/"
-              exact
-              className="nav-link"
-              activeStyle={activeStyle}
-            >
-              Home
-            </NavLink>
-          </li>
-          <li className="navbar-item">
-            <NavLink
-              to="/CurrentOrder"
-              activeStyle={activeStyle}
-              className="nav-link"
-            >
-              Current Order
-            </NavLink>
-          </li>
-          <li className="navbar-item">
-            <NavLink
-              to="/OrderHistory"
-              activeStyle={activeStyle}
-              className="nav-link"
-            >
-              Order History
-            </NavLink>
-          </li>
-        </ul>
-      </div>
+      </NavLink>
+      <ul className="navbar-nav mr-auto">
+        <li className="navbar-item"></li>
+        <li className="navbar-item">
+          <NavLink
+            to="/CurrentOrder"
+            activeStyle={activeStyle}
+            className="nav-link"
+          >
+            Current Order
+          </NavLink>
+        </li>
+        <li className="navbar-item">
+          <NavLink
+            to="/OrderHistory"
+            activeStyle={activeStyle}
+            className="nav-link"
+          >
+            Order History
+          </NavLink>
+        </li>
+      </ul>
     </nav>
   );
 };
