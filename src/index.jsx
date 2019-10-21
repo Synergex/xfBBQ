950043b310197801737d8deb61fad882db307ee2
Johnson Luong <johnson.luong@synergex.com>
2019-10-21 11:50:33 -0700

    added redux store and spinners

--- src/index.jsx
+++ src/index.jsx
@@ -4,10 +4,16 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.scss";
 import App from "./App";
 import "./index.css";
+import configureStore from "./redux/configureStore";
+import { Provider } from "react-redux";
+
+const store = configureStore();
 
 render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("app")
 );
---
+++ src/redux/configureStore.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./reducers/rootReducer";
+import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+import thunk from "redux-thunk";
+
+export default function configureStore(initialState) {
+  // add support for redux dev tools
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+  return createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
+  );
+}
