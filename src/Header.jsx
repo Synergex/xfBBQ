932f8abba97d85a2bb89e9851dc3d8c4b46d9e1c
Johnson Luong <johnson.luong@synergex.com>
2019-10-21 14:29:06 -0700

    fixed viewport scaling issue

--- src/Header.jsx
+++ src/Header.jsx
@@ -9,27 +9,29 @@ const Header = () => {
       <NavLink to="/" exact className="navbar-brand">
         <i>xf</i>BBQ
       </NavLink>
-      <ul className="navbar-nav mr-auto">
-        <li className="navbar-item"></li>
-        <li className="navbar-item">
-          <NavLink
-            to="/CurrentOrder"
-            activeStyle={activeStyle}
-            className="nav-link"
-          >
-            Current Order
-          </NavLink>
-        </li>
-        <li className="navbar-item">
-          <NavLink
-            to="/OrderHistory"
-            activeStyle={activeStyle}
-            className="nav-link"
-          >
-            Order History
-          </NavLink>
-        </li>
-      </ul>
+      <div id="navbarColor01" style={{}}>
+        <ul className="navbar-nav mr-auto">
+          <li className="navbar-item"></li>
+          <li className="navbar-item">
+            <NavLink
+              to="/CurrentOrder"
+              activeStyle={activeStyle}
+              className="nav-link"
+            >
+              Current Order
+            </NavLink>
+          </li>
+          <li className="navbar-item">
+            <NavLink
+              to="/OrderHistory"
+              activeStyle={activeStyle}
+              className="nav-link"
+            >
+              Order History
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
