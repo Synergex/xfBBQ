e00f93eff34ebdc089490955289542bcd81fffff
ElDugano <48536884+ElDugano@users.noreply.github.com>
2019-11-07 16:58:33 -0800

    Removed the onClick

--- src/Header.jsx
+++ src/Header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
         <i>xf</i>BBQ
       </NavLink>
           <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon" onClick="this.tester()"></span>
+              <span className="navbar-toggler-icon"></span>
           </button>
       <div className="collapse navbar-collapse" id="navbarColor01" style={{}}>
         <ul className="navbar-nav mr-auto">
