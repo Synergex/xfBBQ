c47680ff2dbf1290cc8822374fa3052a5e5ba8b8
Johnson Luong <johnson.luong@synergex.com>
2019-11-11 17:10:03 -0800

    change the header based on the user type logged in

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,95 +7,115 @@ export default function Header() {
   const activeStyle = { color: "#5bffd5" };
   const login = useSelector(state => state.login);
 
-  return isEmpty(login) ? (
+  return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <NavLink to="/" exact className="navbar-brand">
-        <i>xf</i>BBQ
-      </NavLink>
-    </nav>
-  ) : (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <NavLink to="/" exact className="navbar-brand">
-        <i>xf</i>BBQ
-      </NavLink>
-      <button
-        className="navbar-toggler collapsed"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarColor01"
-        aria-controls="navbarColor01"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarColor01" style={{}}>
-        <ul className="navbar-nav mr-auto">
-          <li className="navbar-item">
-            <NavLink
-              to="/CurrentOrder"
-              activeStyle={activeStyle}
-              className="nav-link"
-            >
-              Current Order
-            </NavLink>
-          </li>
-          <li className="navbar-item">
-            <NavLink
-              to="/UserRegistrationForm"
-              activeStyle={activeStyle}
-              className="nav-link"
-            >
-              New User Registration
-            </NavLink>
-          </li>
-          <li className="navbar-item">
-            <NavLink
-              to="/UsersList"
-              activeStyle={activeStyle}
-              className="nav-link"
-            >
-              Users
-            </NavLink>
-          </li>
-          <li className="navbar-item">
-            <NavLink
-              to="/BBQRegistrationForm"
-              activeStyle={activeStyle}
-              className="nav-link"
-            >
-              Create New BBQ
-            </NavLink>
-          </li>
-          <li className="navbar-item">
-            <NavLink
-              to="/BBQList"
-              activeStyle={activeStyle}
-              className="nav-link"
-            >
-              BBQs
-            </NavLink>
-          </li>
-          <li className="navbar-item">
-            <NavLink
-              to="/NewOrderForm"
-              activeStyle={activeStyle}
-              className="nav-link"
-            >
-              Place an Order
-            </NavLink>
-          </li>
-          <li className="navbar-item">
-            <NavLink
-              to="/OrderHistory"
-              activeStyle={activeStyle}
-              className="nav-link"
-            >
-              Order History
-            </NavLink>
-          </li>
-        </ul>
-      </div>
+      {isEmpty(login) ? (
+        <NavLink to="/" exact className="navbar-brand">
+          <i>xf</i>BBQ
+        </NavLink>
+      ) : (
+        <>
+          <NavLink to="/" exact className="navbar-brand">
+            <i>xf</i>BBQ
+          </NavLink>
+          <button
+            className="navbar-toggler collapsed"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarColor01"
+            aria-controls="navbarColor01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse"
+            id="navbarColor01"
+            style={{}}
+          >
+            <ul className="navbar-nav mr-auto">
+              <li className="navbar-item">
+                <NavLink
+                  to="/CurrentOrder"
+                  activeStyle={activeStyle}
+                  className="nav-link"
+                >
+                  Current Order
+                </NavLink>
+              </li>
+              {login.type === "Administrator" ? (
+                <>
+                  <li className="navbar-item">
+                    <NavLink
+                      to="/UserRegistrationForm"
+                      activeStyle={activeStyle}
+                      className="nav-link"
+                    >
+                      New User Registration
+                    </NavLink>
+                  </li>
+                  <li className="navbar-item">
+                    <NavLink
+                      to="/UsersList"
+                      activeStyle={activeStyle}
+                      className="nav-link"
+                    >
+                      Users
+                    </NavLink>
+                  </li>
+                </>
+              ) : (
+                <></>
+              )}
+
+              {login.type !== "Attendee" ? (
+                <>
+                  <li className="navbar-item">
+                    <NavLink
+                      to="/BBQRegistrationForm"
+                      activeStyle={activeStyle}
+                      className="nav-link"
+                    >
+                      Create New BBQ
+                    </NavLink>
+                  </li>
+                  <li className="navbar-item">
+                    <NavLink
+                      to="/BBQList"
+                      activeStyle={activeStyle}
+                      className="nav-link"
+                    >
+                      BBQs
+                    </NavLink>
+                  </li>
+                </>
+              ) : (
+                <></>
+              )}
+
+              <li className="navbar-item">
+                <NavLink
+                  to="/NewOrderForm"
+                  activeStyle={activeStyle}
+                  className="nav-link"
+                >
+                  Place an Order
+                </NavLink>
+              </li>
+              <li className="navbar-item">
+                <NavLink
+                  to="/OrderHistory"
+                  activeStyle={activeStyle}
+                  className="nav-link"
+                >
+                  Order History
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </>
+      )}
     </nav>
   );
 }
