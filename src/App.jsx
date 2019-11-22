ed119859f8e8322f501608ccfe0ffa4a789f44c7
ElDugano <48536884+ElDugano@users.noreply.github.com>
2019-11-22 14:55:40 -0800

    First publish of the new order form. Submit is non functional.

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,8 @@ import OrderHistory from "./components/orders/OrderHistory";
 
 import LogoutPage from "./components/login/UserLogout";
 
+import AlphaOrderForm from "./components/orders/AlphaOrderForm";
+
 export default function App() {
   const login = useSelector(state => state.login);
 
@@ -49,6 +51,7 @@ export default function App() {
             path="/UserRegistrationForm"
             component={UserRegistrationForm}
           />
+          <Route path="/AlphaOrderForm" component={AlphaOrderForm} />
           <Route path="/UsersList" component={UserList} />
           <Route path="/BBQRegistrationForm" component={BBQRegistrationForm} />
           <Route path="/BBQList" component={BBQList} />
