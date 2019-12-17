c02d1f1af8d41d6797fa75812bd4959480948c6f
Johnson Luong <johnson.luong@synergex.com>
2019-12-16 16:35:37 -0800

    added service worker support

--- src/index.jsx
+++ src/index.jsx
@@ -8,6 +8,12 @@ import { store, persistor } from "./redux/configureStore";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import Spinner from "./Spinner";
+import * as serviceWorker from "./serviceWorker";
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
 
 render(
   <Provider store={store}>
