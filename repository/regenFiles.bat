111cf33cfae0d70b7be9700c00194f527e5de090
Johnson Luong <johnson.luong@synergex.com>
2020-01-10 17:14:49 -0800

    updated repository and sample data

---
+++ repository/regenFiles.bat
@@ -0,0 +1,6 @@
+createfile -f BBQ -r
+createfile -f Order -r
+createfile -f User -r
+"C:\Program Files\Synergex\SynergyDE\dbl\bin\ipar.exe" -gx BBQ.ism
+"C:\Program Files\Synergex\SynergyDE\dbl\bin\ipar.exe" -gx Order.ism
+"C:\Program Files\Synergex\SynergyDE\dbl\bin\ipar.exe" -gx User.ism
