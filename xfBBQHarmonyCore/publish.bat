18f65241272bc077778ff96ccf85767e1bbee477
Johnson Luong <johnson.luong@synergex.com>
2020-01-13 11:53:16 -0800

    rejiggered SolutionDir and fixed publish.bat to actually work

--- xfBBQHarmonyCore/publish.bat
+++ xfBBQHarmonyCore/publish.bat
@@ -7,16 +7,21 @@ pushd "%SolutionDir%"
 set rpsmfil=%SolutionDir%Repository\bin\Debug\rpsmain.ism
 set rpstfil=%SolutionDir%Repository\bin\Debug\rpstext.ism
 
-set DeployDir=%SolutionDir%PUBLISH
-if not exist %DeployDir%\. mkdir %DeployDir%
+set DeployDir=%SolutionDir%build
+if exist %DeployDir% rmdir /S /Q %DeployDir%
+mkdir %DeployDir%
 
 pushd Services.Host
-dotnet publish -c Debug -r win7-x64 -o %DeployDir%
+dotnet publish -c Debug -r win10-x64 -o %DeployDir%
 popd
 
+copy /Y "C:\Program Files\WindowsApps\Microsoft.VCLibs.140.00_14.0.27323.0_x64__8wekyb3d8bbwe\concrt140_app.dll"    %DeployDir%
+copy /Y "C:\Program Files\WindowsApps\Microsoft.VCLibs.140.00_14.0.27323.0_x64__8wekyb3d8bbwe\msvcp140_app.dll"     %DeployDir%
+copy /Y "C:\Program Files\WindowsApps\Microsoft.VCLibs.140.00_14.0.27323.0_x64__8wekyb3d8bbwe\vcruntime140_app.dll" %DeployDir%
+
 rem Copy in the TraditionalBridge files
-copy TraditionalBridge\bin\Debug\x64\TraditionalBridgeHost.dbr  %DeployDir%
-copy TraditionalBridge\bin\Debug\x64\launch.bat                 %DeployDir%
+copy /Y TraditionalBridge\bin\Debug\x64\TraditionalBridgeHost.dbr  %DeployDir%
+copy /Y TraditionalBridge\bin\Debug\x64\launch.bat                 %DeployDir%
 
 popd
 endlocal
