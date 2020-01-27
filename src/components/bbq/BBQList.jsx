cf9b74c4d5cb24bd29f6cbbe6484f3f7e20dfd07
Johnson Luong <johnson.luong@synergex.com>
2020-01-27 12:57:18 -0800

    bbqlist puts the next bbq on top

--- src/components/bbq/BBQList.jsx
+++ src/components/bbq/BBQList.jsx
@@ -4,14 +4,29 @@ import { loadBBQs } from "../../redux/actions/bbqActions";
 import Spinner from "../../Spinner";
 import BBQTable from "./BBQTable";
 import { Link } from "react-router-dom";
+import moment from "moment";
 
+let nextBBQ;
+let bbqList;
 export default function BBQList() {
   document.title = "𝘹𝘧BBQ - BBQs";
 
   const dispatch = useDispatch();
 
   const bbqs = useSelector(state => state.bbqs);
-  if (bbqs.length === 0) dispatch(loadBBQs());
+  if (Array.isArray(bbqs)) dispatch(loadBBQs());
+  else {
+    // Get closest upcoming BBQ
+    const rightNow = parseInt(moment().format("X"));
+    if (bbqs.value.length > 0) {
+      bbqList = { ...bbqs };
+      nextBBQ = bbqList.value
+        .filter(k => k.Helddate >= rightNow)
+        .sort((a, b) => a.Helddate - b.Helddate)[0];
+      bbqList.value = bbqList.value.filter(bbq => bbq.Id !== nextBBQ.Id);
+      bbqList.value.unshift(nextBBQ);
+    }
+  }
 
   return (
     <div className="jumbotron">
@@ -21,7 +36,7 @@ export default function BBQList() {
         </button>
       </Link>
       <h2>BBQs</h2>
-      {bbqs.length <= 0 ? <Spinner /> : <BBQTable bbqs={bbqs} />}
+      {Array.isArray(bbqs) ? <Spinner /> : <BBQTable bbqs={bbqList} />}
     </div>
   );
 }
